355faf85775904bd9647c19faa142142dba5752d
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-26 17:17:56 +0900

    core

---
+++ core/core.py
@@ -0,0 +1,24 @@
+import jwt
+
+from django.http import JsonResponse
+
+from my_settings import SECRET_KEY
+from .models     import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
+            user         = User.objects.get(email=payload['email'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=400)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
